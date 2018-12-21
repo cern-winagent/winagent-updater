9d3e60c5ed77e5fb77e624345db25b04dc3a4e8b
Esteban Puentes <esteban.puentes@cern.ch>
2018-12-21 15:17:58 +0100

    Create base download functionality

---
+++ winagent-updater/GitHubRelease.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace winagent_updater
+{
+    class GitHubRelease
+    {
+        #region Nested class to identify assets
+        public class GitHubAsset
+        {
+            [JsonProperty(PropertyName = "name")]
+            public string Filename { get; set; }
+        
+            [JsonProperty(PropertyName = "browser_download_url")]
+            public string Url { get; set; }
+        }
+        #endregion
+
+        [JsonProperty(PropertyName = "tag_name")]
+        public string Version { get; set; }
+
+        [JsonProperty(PropertyName = "assets")]
+        public List<GitHubAsset> Files { get; set; }
+    }
+}
---
+++ winagent-updater/Updater.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+using RestSharp.Extensions;
+namespace winagent_updater
+{
+    class Updater
+    {
+        static void Main(string[] args)
+        {
+            CheckUpdates();
+            Console.ReadKey();
+        }
+
+        static int CheckUpdates()
+        {
+            var client = new RestClient("https://api.github.com/repos/cern-winagent/");
+            var downladClient = new RestClient("https://github.com/cern-winagent/");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            // var request = new RestRequest("resource/{id}", Method.POST);
+            var request = new RestRequest("plugin/releases/latest", Method.GET);
+
+            // Async request
+            client.ExecuteAsync(request, response => {
+                GitHubRelease release = Newtonsoft.Json.JsonConvert.DeserializeObject<GitHubRelease>(response.Content);
+                Console.WriteLine(release.Version);
+                if (true)
+                {
+                    foreach (GitHubRelease.GitHubAsset file in release.Files)
+                    {
+                        var downloadRequest = new RestRequest("https://github.com/cern-winagent/plugin/releases/download/v1.0.0/plugin.dll", Method.GET);
+
+                        downladClient.DownloadData(downloadRequest).SaveAs(file.Filename);
+                    }
+                }
+                Console.WriteLine(release.Files[0].Filename);
+
+                var dictionary = release.Files.ToDictionary(x => x.Filename, x => x.Url);
+                //Console.WriteLine(dictionary.Keys);
+                //foreach (string a in response.Data.Assets)
+                //Console.WriteLine(a);
+                foreach (KeyValuePair<string, string> kvp in dictionary)
+                {
+                    //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+                    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+                }
+            });
+                       
+            //https://github.com/cern-winagent/plugin/releases/latest
+
+            return 0;
+        }
+    }
+}
