7ff7bcd528767d35e1f19d1986c43f6d85715678
Esteban Puentes <esteban.puentes@cern.ch>
2019-01-14 14:25:49 +0100

    Compare versions & refactor

--- winagent-updater/Updater.cs
+++ winagent-updater/Updater.cs
@@ -17,38 +17,41 @@ namespace winagent_updater
         {
             var client = new RestClient("https://api.github.com/repos/cern-winagent/");
             var downladClient = new RestClient("https://github.com/cern-winagent/");
-            // client.Authenticator = new HttpBasicAuthenticator(username, password);
 
             // var request = new RestRequest("resource/{id}", Method.POST);
             var request = new RestRequest("plugin/releases/latest", Method.GET);
 
             // Async request
             client.ExecuteAsync(request, response => {
+
+                // Get Info
                 GitHubRelease release = Newtonsoft.Json.JsonConvert.DeserializeObject<GitHubRelease>(response.Content);
                 Console.WriteLine(release.Version);
-                if (true)
-                {
-                    foreach (GitHubRelease.GitHubAsset file in release.Files)
-                    {
-                        var downloadRequest = new RestRequest("https://github.com/cern-winagent/plugin/releases/download/v1.0.0/plugin.dll", Method.GET);
 
-                        downladClient.DownloadData(downloadRequest).SaveAs(file.Filename);
+                // Compare Versions
+                // Latest Version
+                var latestVersion = new Version(release.Version);
+
+                // CurrentVersion
+                var currentVersion = new Version("0.0.1");
+
+                // If latestVersion is grather than currentVersion
+                if(latestVersion.CompareTo(currentVersion) > 0)
+                {
+                    // Create dictionary with filenames and download URLs
+                    var dictionary = release.Files.ToDictionary(x => x.Filename, x => x.Url);
+
+                    // For each pair
+                    foreach (KeyValuePair<string, string> kvp in dictionary)
+                    {
+                        // Download from URL
+                        var downloadRequest = new RestRequest(kvp.Value, Method.GET);
+
+                        // Save as filename
+                        downladClient.DownloadData(downloadRequest).SaveAs(kvp.Key);
                     }
                 }
-                Console.WriteLine(release.Files[0].Filename);
-
-                var dictionary = release.Files.ToDictionary(x => x.Filename, x => x.Url);
-                //Console.WriteLine(dictionary.Keys);
-                //foreach (string a in response.Data.Assets)
-                //Console.WriteLine(a);
-                foreach (KeyValuePair<string, string> kvp in dictionary)
-                {
-                    //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-                    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-                }
             });
-                       
-            //https://github.com/cern-winagent/plugin/releases/latest
 
             return 0;
         }
