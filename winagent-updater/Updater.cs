e55983dc0aeb0c742c8fa78e10637e12b999fef2
Esteban Puentes <esteban.puentes@cern.ch>
2019-01-17 09:21:30 +0100

    Manage filenames

--- winagent-updater/Updater.cs
+++ winagent-updater/Updater.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using RestSharp;
 using RestSharp.Extensions;
 namespace winagent_updater
@@ -16,7 +18,6 @@ namespace winagent_updater
         static int CheckUpdates()
         {
             var client = new RestClient("https://api.github.com/repos/cern-winagent/");
-            var downladClient = new RestClient("https://github.com/cern-winagent/");
 
             // var request = new RestRequest("resource/{id}", Method.POST);
             var request = new RestRequest("plugin/releases/latest", Method.GET);
@@ -41,19 +42,70 @@ namespace winagent_updater
                     // Create dictionary with filenames and download URLs
                     var dictionary = release.Files.ToDictionary(x => x.Filename, x => x.Url);
 
-                    // For each pair
-                    foreach (KeyValuePair<string, string> kvp in dictionary)
+                    // Download attempts (5)
+                    // Download if it fails
+                    int retries = 0;
+                    if (!Download(dictionary) && retries < 5)
                     {
-                        // Download from URL
-                        var downloadRequest = new RestRequest(kvp.Value, Method.GET);
-
-                        // Save as filename
-                        downladClient.DownloadData(downloadRequest).SaveAs(kvp.Key);
+                        retries++;
+                        Console.WriteLine("Failed. Retrying");
+                        Download(dictionary);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Downloaded");
                     }
                 }
             });
 
             return 0;
+        }
+
+        static private Boolean Download(Dictionary<string,string> dictionary)
+        {
+            var downladClient = new RestClient("https://github.com/cern-winagent/");
+
+            // For each pair -> Download
+            foreach (KeyValuePair<string, string> kvp in dictionary)
+            {
+                // Download from URL
+                var downloadRequest = new RestRequest(kvp.Value, Method.GET);
+
+                // Save as filename
+                downladClient.DownloadData(downloadRequest).SaveAs(kvp.Key);
+            }
+
+            // Check checksum
+            foreach (KeyValuePair<string, string> kvp in dictionary)
+            {
+                // If it's not a checksumfile
+                if (Path.GetExtension(kvp.Key)!=".sha1")
+                {
+                    // Calculate and compare with the pertinent checksum
+                    if (CalculateChecksum(kvp.Key) != ReadChecksum(kvp.Key + ".sha1"))
+                    {
+                        // Fail if checksums doesn't match
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+
+        static private string CalculateChecksum(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (var cryptoProvider = new SHA1CryptoServiceProvider())
+            {
+                return BitConverter.ToString(cryptoProvider.ComputeHash(bs)).Replace("-", string.Empty).ToLower();
+            }
+        }
+
+        static private string ReadChecksum(string filePath)
+        {
+            return File.ReadAllText(filePath).Split(' ')[0];
         }
     }
 }
