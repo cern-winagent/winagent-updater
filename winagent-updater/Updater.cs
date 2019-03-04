db10d879c70880aa266ed352d897d67814208bad
Esteban Puentes <esteban.puentes@cern.ch>
2019-03-04 10:22:03 +0100

    Update Event Logs

--- winagent-updater/Updater.cs
+++ winagent-updater/Updater.cs
@@ -15,12 +15,12 @@ namespace winagent_updater
     {
         static void Main(string[] args)
         {
+            // Delay before the first update
+            // This avoids errors when the service starts
+            Thread.Sleep(10000);
+
             while (true)
             {
-                // Delay before the first update
-                // This avoids errors when the service starts
-                Thread.Sleep(10000);
-
                 // Main functionality
                 CheckUpdates();
 
@@ -54,12 +54,20 @@ namespace winagent_updater
                     using (EventLog eventLog = new EventLog("Application"))
                     {
                         // EventID 2 => General error
+                        System.Text.StringBuilder message = new System.Text.StringBuilder("General Error");
+                        message.Append(Environment.NewLine);
+                        message.Append("Response StatusCode: ");
+                        message.Append(response.StatusCode);
+                        message.Append(Environment.NewLine);
+                        message.Append("Error Message: ");
+                        message.Append(response.ErrorMessage);
+                        message.Append(Environment.NewLine);
+                        message.Append("Content: ");
+                        message.Append(response.Content);
+
                         eventLog.Source = "WinagentUpdater";
                         eventLog.WriteEntry("General error", EventLogEntryType.Error, 2, 1);
-                        eventLog.WriteEntry("Response StatusCode: " + response.StatusCode, EventLogEntryType.Error, 2, 1);
-                        eventLog.WriteEntry("Error Message: " + response.ErrorMessage, EventLogEntryType.Error, 2, 1);
-                        eventLog.WriteEntry("Error Exception: " + response.ErrorException, EventLogEntryType.Error, 2, 1);
-                        eventLog.WriteEntry("Content: " + response.Content, EventLogEntryType.Error, 2, 1);
+                        eventLog.WriteEntry(message.ToString(), EventLogEntryType.Error, 2, 1);
                     }
                 }
             }
@@ -68,9 +76,11 @@ namespace winagent_updater
                 using (EventLog eventLog = new EventLog("Application"))
                 {
                     // EventID 1 => Request failed
+                    System.Text.StringBuilder message = new System.Text.StringBuilder("Request failed: ");
+                    message.Append(response.StatusCode);
+
                     eventLog.Source = "WinagentUpdater";
-                    eventLog.WriteEntry("Request failed", EventLogEntryType.Error, 1, 1);
-                    eventLog.WriteEntry(response.ErrorMessage, EventLogEntryType.Error, 1, 1);
+                    eventLog.WriteEntry(message.ToString(), EventLogEntryType.Error, 1, 1);
                 }
             }
         }
@@ -100,7 +110,16 @@ namespace winagent_updater
                 // Create dictionary with filenames and download URLs
                 var dictionary = release.Files.ToDictionary(x => x.Filename, x => x.Url);
 
-                // TODO: Catch if it does not exist
+                /* TODO: For a 5 days test (every 40 seconds) it failed with
+                 *  Service not found: 
+                 *  System.InvalidOperationException: Cannot stop Winagent service on computer '.'. ---System.ComponentModel.Win32Exception: The service has not been started
+                 *      --- End of inner exception stack trace ---
+                 *      at System.ServiceProcess.ServiceController.Stop()
+                 *     at winagent_updater.Updater.ProcessResponse(String responseContent)
+                 *
+                 * EVEN WITH THE TRY-CATCH
+                 */
+
                 // Get the service by name
                 try
                 {
@@ -118,10 +137,18 @@ namespace winagent_updater
                     // Start the service
                     serviceController.Start();
                 }
-                catch 
+                catch (Exception e)
                 {
-                    // TODO: remove
-                    Console.WriteLine("test");
+                    using (EventLog eventLog = new EventLog("Application"))
+                    {
+                        // EventID 3 => Service not started
+                        System.Text.StringBuilder message = new System.Text.StringBuilder("Service not started: ");
+                        message.Append(Environment.NewLine);
+                        message.Append(e.ToString());
+
+                        eventLog.Source = "WinagentUpdater";
+                        eventLog.WriteEntry(message.ToString(), EventLogEntryType.Error, 3, 1);
+                    }
                 }
             }
         }
@@ -138,7 +165,24 @@ namespace winagent_updater
 
                 // Save as filename
                 // Save in the right folder
-                downladClient.DownloadData(downloadRequest).SaveAs(kvp.Key);
+
+                try
+                {
+                    downladClient.DownloadData(downloadRequest).SaveAs(kvp.Key);
+                }
+                catch(ArgumentNullException ane)
+                {
+                    using (EventLog eventLog = new EventLog("Application"))
+                    {
+                        // EventID 4 => Could not write the file
+                        System.Text.StringBuilder message = new System.Text.StringBuilder("File could not be saved: ");
+                        message.Append(Environment.NewLine);
+                        message.Append(ane.ToString());
+
+                        eventLog.Source = "WinagentUpdater";
+                        eventLog.WriteEntry(message.ToString(), EventLogEntryType.Error, 4, 1);
+                    }
+                }
             }
         }
 
